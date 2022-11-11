a1546c6776dbd34bf2edb42ff93e9d40b3fbe065
sathwik17 <adityasathwikdv1772003@gmail.com>
2022-11-11 21:08:21 +0530

    nav2

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,15 +7,47 @@ const Navbar = () => {
         <div>
    <h1 className=' text-5xl ml-2 font-bold'>ADITYA <span className='text-yellow-400'>SATHWIK</span></h1>
    </div>
-   
-   <ul className="flex px-3" >
-    <li className='px-5 cursor-pointer text-gray-500 '><a>Home</a></li>
-    <li className='px-5 cursor-pointer text-gray-500 '><a>About</a></li>
-    <li className='px-5 cursor-pointer text-gray-500 '><a>Skills</a></li>
-    <li className='px-5 cursor-pointer text-gray-500 '><a>Experience</a></li>
-    <li className='px-5 cursor-pointer text-gray-500 '><a>Projects</a></li>
-    <li className='px-5 cursor-pointer text-gray-500 '><a>Connect with me</a></li>
+   <ul>
+    <li className="list">
+        <a href="#">
+            <span className="icon"></span>
+            <span className="text">Home</span>
+        </a>
+    </li>
+    <li className="list">
+        <a href="#">
+            <span className="icon"></span>
+            <span className="text">Home</span>
+        </a>
+    </li>
+    <li className="list">
+        <a href="#">
+            <span className="icon"></span>
+            <span className="text">Home</span>
+        </a>
+    </li>
+    <li className="list">
+        <a href="#">
+            <span className="icon"></span>
+            <span className="text">Home</span>
+        </a>
+    </li>
+    <li className="list">
+        <a href="#">
+            <span className="icon"></span>
+            <span className="text">Home</span>
+        </a>
+    </li>
+    <li className="list">
+        <a href="#">
+            <span className="icon"></span>
+            <span className="text">Home</span>
+        </a>
+    </li>
    </ul>
+   
+  
+  
 
     </div>
    
